cfc8dff6a1e32a42135a4a5b500833a4ad31d4dc
Edim Dendy <edimdendy@gmail.com>
2020-12-09 20:47:49 +0700

    implemnet serviceworker

--- src/scripts/data/restaurant-source.js
+++ src/scripts/data/restaurant-source.js
@@ -1,4 +1,3 @@
-import CONFIG from '../globals/config';
 import API_ENDPOINT from '../globals/api-endpoint';
 
 class RestaurantSource {
@@ -12,19 +11,6 @@ class RestaurantSource {
     const response = await fetch(API_ENDPOINT.DETAIL(id));
     const responseJson = await response.json();
     return responseJson.restaurant;
-  }
-
-  static async addReviewRestaurant(data) {
-    const response = await fetch(API_ENDPOINT.POST_REVIEW, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Auth-Token': CONFIG.KEY,
-      },
-      body: JSON.stringify(data),
-    });
-    const responseJson = await response.json();
-    return responseJson;
   }
 
   static async picture(size, pictureId) {
