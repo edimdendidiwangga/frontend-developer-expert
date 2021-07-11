0103caf1d9eeb2156835a1f588ea7d4d47bb7695
Edim Ss <edim@softwareseni.com>
2021-07-12 06:04:51 +0700

    implement E2E Testing

---
+++ e2e/Liking_Restaurant.spec.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+
+Feature('Liking_Restaurant');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
+
+Scenario('Showing empty liked restaurant', ({ I }) => {
+  I.see('Favorite Restaurant', 'h1');
+  I.see('Empty Favorite Restaurant', 'h3');
+});
+
+Scenario('Liking restaurant', async ({ I }) => {
+  I.see('Empty Favorite Restaurant', 'h3');
+
+  I.amOnPage('/');
+  I.seeElement('.card-content a.link');
+
+  const firstRestaurant = locate('.card-content a.link .card-title').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(locate('.card-content a.link').first());
+
+  const detailUrl = await I.grabAttributeFrom(locate('.card-content a.link').first(), 'href');
+  I.amOnPage(detailUrl);
+
+  I.seeElement('.favorite-button');
+  I.click('.favorite-button');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.card-content');
+  const likedRestaurantTitle = await I.grabTextFrom('.card-content .card-title');
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+});
+
+Scenario('Unliking restaurant', async ({ I }) => {
+  I.see('Empty Favorite Restaurant', 'h3');
+
+  I.amOnPage('/');
+  I.seeElement('.card-content a.link');
+
+  const firstRestaurant = locate('.card-content a.link').first();
+  I.click(firstRestaurant);
+
+  const detailUrl = await I.grabAttributeFrom(locate('.card-content a.link').first(), 'href');
+  I.amOnPage(detailUrl);
+
+  I.seeElement('.favorite-button');
+  I.click('.favorite-button');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.card-content');
+
+  I.click(firstRestaurant);
+
+  I.amOnPage(detailUrl);
+
+  I.seeElement('.favorite-button');
+  I.click('.favorite-button');
+
+  I.amOnPage('/#/favorite');
+  I.see('Empty Favorite Restaurant', 'h3');
+});
